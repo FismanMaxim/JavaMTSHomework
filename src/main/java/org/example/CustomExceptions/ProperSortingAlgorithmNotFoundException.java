77d44124a4d85547c8ca80bced22d748f67ead56
Maxim <mfisman05@gmail.com>
2023-10-20 21:58:19 +0300

    Deleted

--- src/main/java/org/example/CustomExceptions/ProperSortingAlgorithmNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.CustomExceptions;
-
-public class ProperSortingAlgorithmNotFoundException extends RuntimeException {
-  public ProperSortingAlgorithmNotFoundException () {
-    super();
-  }
-}
--- src/main/java/org/example/CustomExceptions/TooLargeListSizeException.java
+++
@@ -1,7 +0,0 @@
-package org.example.CustomExceptions;
-
-public class TooLargeListSizeException extends RuntimeException {
-  public TooLargeListSizeException() {
-    super();
-  }
-}
