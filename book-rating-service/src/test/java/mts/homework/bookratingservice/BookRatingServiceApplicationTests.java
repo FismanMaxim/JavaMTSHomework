ca31ddef106284842c4ba552c17747857b71ba46
Fisman Maxim <mfisman05@gmail.com>
2024-03-26 18:34:24 +0300

    12

---
+++ book-rating-service/src/test/java/mts/homework/bookratingservice/BookRatingServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package mts.homework.bookratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRatingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ book-rating-service/src/test/java/mts/homework/bookratingservice/RatingServiceTest.java
@@ -0,0 +1,105 @@
+package mts.homework.bookratingservice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest(
+    classes = {RatingService.class},
+    properties = {
+      "topic-to-get-get-rating-request=get-rating-request-test",
+      "topic-to-send-get-rating-response=get-rating-response-test"
+    })
+@Import({
+  KafkaAutoConfiguration.class,
+  RatingRepository.class,
+  RatingServiceTest.ObjectMapperTestConfig.class
+})
+@Testcontainers
+class RatingServiceTest {
+  @TestConfiguration
+  static class ObjectMapperTestConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+      return new ObjectMapper();
+    }
+  }
+
+  @Container @ServiceConnection
+  public static final KafkaContainer KAFKA =
+      new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
+
+  @Autowired private RatingService ratingService;
+  @Autowired private ObjectMapper objectMapper;
+
+  @Test
+  void shouldSendBookRatingSuccessfully() {
+    assertDoesNotThrow(() -> ratingService.sendGetRatingResponse(67L, 10));
+
+    KafkaTestConsumer consumer =
+        new KafkaTestConsumer(KAFKA.getBootstrapServers(), "some-group-id");
+    consumer.subscribe(List.of("get-rating-response-test"));
+
+    ConsumerRecords<String, String> records = consumer.poll();
+    assertEquals(1, records.count());
+    records
+        .iterator()
+        .forEachRemaining(
+            record -> {
+              RatingService.CalculateBookRatingResponse message = null;
+              try {
+                message =
+                    objectMapper.readValue(
+                        record.value(), RatingService.CalculateBookRatingResponse.class);
+              } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+              }
+              assertEquals(67L, message.bookId());
+            });
+  }
+}
+
+class KafkaTestConsumer {
+
+  private final KafkaConsumer<String, String> consumer;
+
+  public KafkaTestConsumer(String bootstrapServers, String groupId) {
+    Properties props = new Properties();
+
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+    this.consumer = new KafkaConsumer<>(props);
+  }
+
+  public void subscribe(List<String> topics) {
+    consumer.subscribe(topics);
+  }
+
+  public ConsumerRecords<String, String> poll() {
+    return consumer.poll(Duration.ofSeconds(5));
+  }
+}
